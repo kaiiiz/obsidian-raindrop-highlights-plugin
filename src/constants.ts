e2cd7a8de35c4039678b53e97e4cdfc0eda81366
kaiiiz <ukaizheng@gmail.com>
2022-09-16 20:30:03 +0800

    Bump version to v0.0.14

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import DEFAULT_TEMPLATE from './assets/defaultTemplate.njk';
 import type { RaindropPluginSettings } from "./types";
 
-export const VERSION = '0.0.13';
+export const VERSION = '0.0.14';
 
 export const DEFAULT_SETTINGS: RaindropPluginSettings = {
 	version: VERSION,
