111e267b0c4b2131b4cd5fdf56c5af0697ee8b34
kaiiiz <ukaizheng@gmail.com>
2024-04-15 03:34:19 +0800

    chores: add breaking change notice

--- src/modal/breakingChange.ts
+++ src/modal/breakingChange.ts
@@ -15,6 +15,14 @@ export default class BreadkingChangeModal extends Modal {
 		this.titleEl.innerText = "Raindrop Highlight - Breaking Changes";
 
 		let breakingChanges = "";
+		if (semver.lt(currentVersion, "0.0.21")) {
+			breakingChanges += `<p>v0.0.21</p>
+<ul>
+<li>Sync collections from last update time is now changed to sync from created time. You should now use either \`Raindrop Highlights: Sync all bookmarks (full sync)\` or \`Raindrop Highlights: Sync this bookmark\` command to update existing files. See issue <a href="https://github.com/kaiiiz/obsidian-raindrop-highlights-plugin/issues/72">#72</a> for details.</li>
+</ul>
+`
+		}
+
 		if (semver.lt(currentVersion, "0.0.19")) {
 			breakingChanges += `<p>v0.0.19</p>
 <ul>
