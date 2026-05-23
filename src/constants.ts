61d642d3672deae7b09ff2ac5e756d19c4a1749d
kaiiiz <ukaizheng@gmail.com>
2026-05-23 17:08:49 +0800

    fix: use default metadata template in init settings
    resolve #100

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 import DEFAULT_TEMPLATE from "./assets/defaultTemplate.njk";
+import DEFAULT_METADATA_TEMPLATE from "./assets/defaultMetadataTemplate.njk";
 import DEFAULT_FILENAME_TEMPLATE from "./assets/defaultFilenameTemplate.njk";
 import type { RaindropCollection, ZPluginSettingsType } from "./types";
 
@@ -21,7 +22,7 @@ export const DEFAULT_SETTINGS: ZPluginSettingsType = {
 	autoSyncNewNestedCollections: false,
 	syncCollections: {},
 	template: DEFAULT_TEMPLATE,
-	metadataTemplate: "",
+	metadataTemplate: DEFAULT_METADATA_TEMPLATE,
 	filenameTemplate: DEFAULT_FILENAME_TEMPLATE,
 	preventMovingExistingFiles: true,
 	autoSyncInterval: 0,
